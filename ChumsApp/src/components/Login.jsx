910128be9b1881e43807d6a8a4667700fbbaffda
Kalondu Muema <30372680+Natmuema@users.noreply.github.com>
2025-11-12 23:04:18 +0300

    Update Login.jsx

--- ChumsApp/src/components/Login.jsx
+++ ChumsApp/src/components/Login.jsx
@@ -65,7 +65,7 @@ const Login = () => {
 
     try {
       // Replace with your actual API endpoint
-      const response = await fetch('http://127.0.0.1:8000/api/login/', {
+      const response = await fetch('https://moneygrow.onrender.com/api/login/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
